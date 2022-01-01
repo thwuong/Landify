8d8dcdfc2defa79b89f87b3576c00eba20ad9280
your name <duongthuong654@gmail.com>
2022-01-01 10:48:29 +0700

    update mobile

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const openMenu = document.querySelector('.menu-icon');
 const menu = document.querySelector('.header__menu');
-
+const closeMenu = document.querySelector(' .mobile-close ');
 function start(){
     fadeMenu();
 }
 function fadeMenu(){
     openMenu.addEventListener('click',function(){
         menu.classList.toggle('open');
-        menu.style.animation =  `fade ${0.4}s ease-in-out`;
+        // menu.style.animation =  `fade ${0.4}s ease-in-out`;
     })
+    closeMenu.addEventListener('click',function(){
+        menu.classList.toggle('open')
+    })
+    
 }
 
 start();
